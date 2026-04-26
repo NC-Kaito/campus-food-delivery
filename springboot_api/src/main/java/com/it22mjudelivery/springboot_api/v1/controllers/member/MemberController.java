bce5a4646a8170bcebbe5cde5809ef94cada087b
natthapong <pkch48rov@gmail.com>
2026-04-26 21:03:40 +0700

    Add Code for Edit Profile Member

--- springboot_api/src/main/java/com/it22mjudelivery/springboot_api/v1/controllers/member/MemberController.java
+++ springboot_api/src/main/java/com/it22mjudelivery/springboot_api/v1/controllers/member/MemberController.java
@@ -28,4 +28,32 @@ public class MemberController {
             return ResponseEntity.internalServerError().body("เกิดข้อผิดพลาดที่ระบบ");
         }
     }
+
+    @PostMapping("/registerMember")
+    public ResponseEntity<?> doRegisterMember(@RequestBody MemberDto memberDto) {
+        try {
+            boolean isResult = memberService.doRegisterMember(memberDto);
+            if (isResult) {
+                return ResponseEntity.ok("สมัครสมาชิกสำเร็จ");
+            }
+            return ResponseEntity.badRequest().body("สมัครสมาชิกไม่สำเร็จ");
+        } catch (RuntimeException e) {
+            // จับข้อความที่เรา throw เช่น "ชื่อผู้ใช้งานนี้ถูกใช้ไปแล้ว"
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            // กรณีเกิด Error อื่นๆ ที่ไม่ได้คาดคิด
+            System.out.println(e);
+            return ResponseEntity.internalServerError().body("เกิดข้อผิดพลาดที่ระบบ");
+        }
+    }
+
+    @GetMapping("/getMember")
+    public ResponseEntity<?> getMemberByUsername(@RequestBody MemberDto memberDto) {
+        Member member = memberService.getMemberByUsername(memberDto.getUsername());
+        if(member != null){
+            return ResponseEntity.ok(member);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ไม่พบผู้ใช้งานนี้");
+        }
+    }
 }
