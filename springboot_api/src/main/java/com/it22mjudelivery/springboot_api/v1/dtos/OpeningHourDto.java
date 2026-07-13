d0fd0751d986d50af6d50d55be3c811f075b984b
natthapong <pkch48rov@gmail.com>
2026-07-13 18:45:14 +0700

    update day&time restaurant table

---
+++ springboot_api/src/main/java/com/it22mjudelivery/springboot_api/v1/dtos/OpeningHourDto.java
@@ -0,0 +1,4 @@
+package com.it22mjudelivery.springboot_api.v1.dtos;
+
+public class OpeningHourDto {
+}
---
+++ springboot_api/src/main/java/com/it22mjudelivery/springboot_api/v1/entities/Restaurantopeninghour.java
@@ -0,0 +1,4 @@
+package com.it22mjudelivery.springboot_api.v1.entities;
+
+public class Restaurantopeninghour {
+}
